fc090902817c142b86f20ed9ab2d8daa537669dd
Mohith A <mohith.a8357@gmail.com>
2026-09-02 09:56:32 +0530

    Add Medium Python Solution for 15. 3Sum from LeetCode

---
+++ LeetCode/Python/Medium/15. 3Sum/Solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        triplet = []
+        n = len(nums)
+
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = n - 1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total == 0:
+                    triplet.append([nums[i], nums[l], nums[r]])
+
+                    while l < r and nums[l + 1] == nums[l]:
+                        l += 1
+
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+
+                    l += 1
+                    r -= 1
+
+                elif total < 0:
+                    l += 1
+
+                else:
+                    r -= 1
+
+        return triplet
