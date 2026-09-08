be0078729da271004a907ed69e6f9e3f5c5a6130
Mohith A <mohith.a8357@gmail.com>
2026-09-08 20:28:44 +0530

    Add Medium Python Solution for 764. N-ary Tree Level Order Traversal from LeetCode

---
+++ LeetCode/Patterns/Trees/764. N-ary Tree Level Order Traversal/Solution.py
@@ -0,0 +1,26 @@
+from collections import deque
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        res = []
+        while queue:
+            level = []
+            level_size = len(queue)
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                for child in node.children:
+                    queue.append(child)
+            res.append(level)
+        return res
