80d0b055ab91d4cd35f3e366e94c330b4b87450a
Mohith A <mohith.a8357@gmail.com>
2026-09-08 12:53:13 +0530

    Add Easy Python Solution for 1. Two Sum from LeetCode

--- LeetCode/Python/Easy/1. Two Sum/README.md
+++ LeetCode/Python/Easy/1. Two Sum/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 1782 ms
+- **Memory:** 18.3 MB
 
 ---
 
--- LeetCode/Python/Easy/1. Two Sum/Solution.py
+++ LeetCode/Python/Easy/1. Two Sum/Solution.py
@@ -1,12 +1,14 @@
+from typing import List
+import ast
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if not nums: return []
-
-        result = []
         for i in range(len(nums)):
-            if nums[i] == target:
-                result.append(nums[i])
-            elif nums[i] < target:
-                result.append(nums[i])
-                for j in range(i+1,len(nums)):
-                    
+            for j in range(i+1,len(nums)):
+                if(nums[j] == target - nums[i]):
+                    return [i,j]
+        return []
+
+
+
+
+        
